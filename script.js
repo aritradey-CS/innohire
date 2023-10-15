0527bbe3699eb69708799a759ee70899af8269f9
aritradey-CS <aritra1999.dey@gmail.com>
2023-10-15 13:50:25 +0530

    locomotive added & vid added .page1

--- script.js
+++ script.js
@@ -38,3 +38,28 @@ function loco() {
   ScrollTrigger.refresh();
 }
 loco();
+
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".page1 h1",
+    scroller: ".main",
+    start: "top 35%",
+    end: "top 0",
+    scrub: 3,
+  },
+});
+tl.to(".page1 h1", {
+  x: -100,
+}, "anim");
+tl.to(".page1 h2", {
+  x: 80,
+},"anim");
+tl.to(".page1 h3", {
+  x:-200,
+},"anim");
+tl.to(".page1 h4", {
+  x:200,
+},"anim");
+tl.to(".page1 video", {
+  width: "90%"
+},"anim");

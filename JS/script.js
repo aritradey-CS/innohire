e319a52fde7f098f76c05d50768b037ab28dbf9b
aritradey-CS <aritra1999.dey@gmail.com>
2023-11-26 21:17:37 +0530

    New components are added and desings are updated

--- JS/script.js
+++ JS/script.js
@@ -204,3 +204,23 @@ function showSuggestions(list){
 }
 
 search ();
+
+function review(){
+  const showContainers = document.querySelectorAll(".show-replies");
+
+showContainers.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    let parentContainer = e.target.closest(".comment__container");
+    let _id = parentContainer.id;
+    if (_id) {
+      let childrenContainer = parentContainer.querySelectorAll(
+        `[dataset=${_id}]`
+      );
+      childrenContainer.forEach((child) => child.classList.toggle("opened"));
+    }
+  })
+);
+
+}
+
+review ();
